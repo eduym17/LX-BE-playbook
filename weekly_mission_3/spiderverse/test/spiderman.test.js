2e4061bba7737e587ac8bcec18ad8ab0b9da32c1
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-20 12:32:29 -0500

    week 3 exercise 2

---
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -0,0 +1,15 @@
+const Spiderman = require('./../app/spiderman')
+describe("Unit tests for Spiderman class", () => {
+  test('1) Create an Spiderman object', () => {
+    const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+    expect(andrewGarfield.name).toBe("Spiderman Sony");
+    expect(andrewGarfield.age).toBe(31);
+    expect(andrewGarfield.actor).toBe("Andrew Garfield");
+    expect(andrewGarfield.movies).toBe(2);
+    expect(andrewGarfield.studio).toBe("Sony");
+  });
+  test('2) Use the method getInfo()', () => {
+    const tommHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+    expect(tommHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
+  })
+})
