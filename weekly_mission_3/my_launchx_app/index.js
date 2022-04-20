4192ed0ef4bf3b735ec06dff42e9d361b56e7b31
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-20 12:09:13 -0500

    week 3: exercise 1

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,6 @@
+const MissionCommander = require('./app/missionCommander');
+const Julio = new MissionCommander('Julio')
+const Carlos = new MissionCommander('Carlos')
+const Eduardo = new MissionCommander('Eduardo')
+
+console.log(Julio, Carlos, Eduardo)
